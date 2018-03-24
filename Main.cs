891e0caf0c203291ba57b0ed155326ee12458b66
HomoRiron <okutsu1108@gmail.com>
2018-03-24 14:50:06 +0900

    AsyncとかTaskとかよくわからないけど使ったらこうなった...

--- Main.cs
+++ Main.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using CoreTweet;
@@ -27,7 +24,8 @@ namespace Twitter2Discord
             Console.ReadLine();
         }
         static void Main(string[] args) => MainAsync().Wait();
-        static async Task SendTweet(SocketMessage msg)
+        static void SendTweet(SocketMessage msg) => SendTweetAsync(msg).Wait();
+        static async Task SendTweetAsync(SocketMessage msg)
         {
             var stream = tokens.Streaming.User();
             foreach (var mes in stream)
@@ -43,38 +41,45 @@ namespace Twitter2Discord
         }
         static async Task Receive(SocketMessage msg)
         {
-            if (!msg.Author.Username.Equals("Twitter2Discord")) { 
+            if (msg.Author.Username.Equals("Twitter2Discord"))
+            {
+                return;
+            }
             switch (msg.Content)
             {
                 case "login":
-                    if (!IsLogin)
-
-                    {
-                        session = OAuth.Authorize(key, secret);
-                        await msg.Channel.SendMessageAsync($"下記URLにアクセスし、認証したら「PIN:認証後に出てきた数字」と送信してください\n\n{session.AuthorizeUri.AbsoluteUri}");
-
-                        IsLogin = true;
-                    }
-                    
-                    break;
-            }
-                if (msg.Content.Contains("PIN:"))
-                {
                     if (IsLogin)
                     {
-                        try
-                        {
-                            tokens = await OAuth.GetTokensAsync(session, msg.Content.Replace("PIN:", ""));
-                            await msg.Channel.SendMessageAsync("ログインに成功しました");
-                            await SendTweet(msg);
-                        }
-                        catch (Exception)
-                        {
-                            await msg.Channel.SendMessageAsync("ログインに失敗しました、正しいPINコードを入力してください");
-                        }
+                        return;
                     }
-                }
+                    session = OAuth.Authorize(key, secret);
+                    await msg.Channel.SendMessageAsync($"下記URLにアクセスし、認証したら「PIN:認証後に出てきた数字」と送信してください\n\n{session.AuthorizeUri.AbsoluteUri}");
+                    IsLogin = true;
+
+                    break;
+                case "ping":
+                    await msg.Channel.SendMessageAsync("pong");
+                    break;
             }
+            if (msg.Content.Contains("PIN:"))
+            {
+                if (!IsLogin)
+                {
+                    return;
+                }
+                try
+                {
+                    tokens = await OAuth.GetTokensAsync(session, msg.Content.Replace("PIN:", ""));
+                    await msg.Channel.SendMessageAsync("ログインに成功しました");
+                    await Task.Run(() => SendTweet(msg));
+                }
+                catch (Exception)
+                {
+                    await msg.Channel.SendMessageAsync("ログインに失敗しました、正しいPINコードを入力してください");
+                }
+
+            }
+
         }
     }
 }
